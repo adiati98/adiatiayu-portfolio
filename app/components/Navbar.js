7b1c130646a23fba361e20124524539748563819
Ayu Adiati <retno.ayu98@gmail.com>
2023-07-20 13:53:40 +0200

    Add footer * Add <nav> in Navbar

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -3,24 +3,26 @@ import Link from "next/link"
 export default function Navbar() {
 	return (
 		<>
-			<div>
-				<h1>
-					<Link href='/'>LOGO/Homepage</Link>
-				</h1>
-			</div>
-			<div>
-				<ul>
-					<li>
-						<Link href='/about'>About</Link>
-					</li>
-					<li>
-						<Link href='/blog'>Blog</Link>
-					</li>
-					<li>
-						<Link href='/contact'>Contact</Link>
-					</li>
-				</ul>
-			</div>
+			<nav>
+				<div>
+					<h1>
+						<Link href='/'>LOGO/Homepage</Link>
+					</h1>
+				</div>
+				<div>
+					<ul>
+						<li>
+							<Link href='/about'>About</Link>
+						</li>
+						<li>
+							<Link href='/blog'>Blog</Link>
+						</li>
+						<li>
+							<Link href='/contact'>Contact</Link>
+						</li>
+					</ul>
+				</div>
+			</nav>
 		</>
 	)
 }
