64dfed96d5b2751aa23cc1f9297a84d1b13eb065
Ayu Adiati <retno.ayu98@gmail.com>
2023-07-20 13:38:20 +0200

    Add navbar * Change metadata in layout.js

---
+++ app/components/Navbar.js
@@ -0,0 +1,26 @@
+import Link from "next/link"
+
+export default function Navbar() {
+	return (
+		<>
+			<div>
+				<h1>
+					<Link href='/'>LOGO/Homepage</Link>
+				</h1>
+			</div>
+			<div>
+				<ul>
+					<li>
+						<Link href='/about'>About</Link>
+					</li>
+					<li>
+						<Link href='/blog'>Blog</Link>
+					</li>
+					<li>
+						<Link href='/contact'>Contact</Link>
+					</li>
+				</ul>
+			</div>
+		</>
+	)
+}
